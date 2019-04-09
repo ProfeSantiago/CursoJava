2d838fda1f4c58148ec38f01dea96caca5406295
Santiago <santiago.rodriguez.paniagua@hotmail.com>
2019-04-09 00:29:06 -0600

    Update Web Services

--- 12) Servicios Web Java/ConectAPI/src/conectapi/ConectAPI.java
+++ 12) Servicios Web Java/ConectAPI/src/conectapi/ConectAPI.java
@@ -1,21 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/** *******************************************************************************
+ * Nota: el *.jar necesario para usar los las liberías de JSon se descarga de aqui:
+ * https://jar-download.com/maven-repository-class-search.php?search_box=javax.json
+ * https://jar-download.com/maven-repository-class-search.php?search_box=org.json
+ *
+ * Ah!, y de aquí se pueden tomar algunas ideas:
+ * https://docs.oracle.com/middleware/1221/wls/WLPRG/java-api-for-json-proc.htm#WLPRG1055
+ * https://www.jokecamp.com/blog/code-examples-api-http-get-json-different-languages/#java
+ ******************************************************************************** */
 package conectapi;
 
-/**
- *
- * @author santiagoRP1979
- */
+//import javax.json.Json;
+//import javax.json.JsonReader;
+//import javax.json.JsonStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ConectAPI {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
     }
-    
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, JSONException {
+        JSONObject json = readJsonFromUrl("https://jsonip.com");
+        System.out.println(json.toString());
+        System.out.println(json.get("ip"));
+    }
+
 }
